8d9e6a7fa3c64a11e3be335291f257946e5ae3e4
manandi-iu <44542974+ManishReddyN@users.noreply.github.com>
2025-02-26 20:09:18 -0500

    removed the comp folder

--- js/loadNavbar.js
+++ js/loadNavbar.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('./components/navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar-container').innerHTML = data;
-            const currentPage = window.location.pathname.split('/').pop();
-            const linkId = {
-                'index.html': 'home-link',
-                'about.html': 'about-link',
-                'add.html': 'add-link',
-                'search.html': 'search-link'
-            }[currentPage];
-            if (linkId) {
-                document.getElementById(linkId).classList.add('active');
-            }
-        })
-        .catch(error => console.error('Error loading navbar:', error));
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("navbar-container").innerHTML = data;
+      const currentPage = window.location.pathname.split("/").pop();
+      const linkId = {
+        "index.html": "home-link",
+        "about.html": "about-link",
+        "add.html": "add-link",
+        "search.html": "search-link",
+      }[currentPage];
+      if (linkId) {
+        document.getElementById(linkId).classList.add("active");
+      }
+    })
+    .catch((error) => console.error("Error loading navbar:", error));
 });
